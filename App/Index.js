dc61ca0acf69b986ddecd6b822662b6a18a1d0ff
JulianGonzalezLopez <94023125+JulianGonzalezLopez@users.noreply.github.com>
2022-04-04 02:24:09 -0300

    Delete App directory

--- App/Index.js
+++
@@ -1,6 +0,0 @@
-import {App} from "./App.js";
-import { Router } from "./components/Router.js";
-
-document.addEventListener("DOMContentLoaded", App);
-window.addEventListener("hashchange", Router);
-
